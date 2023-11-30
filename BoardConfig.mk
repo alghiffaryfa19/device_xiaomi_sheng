bd2429c48599205660f6b470f9a0ea2757b07a1b
Arian <arian.kulmer@web.de>
2023-11-30 19:25:01 +0100

    sheng: Set lmodroid version as fingerprint for mdtoops
    Change-Id: Ie3a663c03279fcc9d9f0d707bbf8ec7a17e4851f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,7 @@ TARGET_NO_KERNEL_OVERRIDE := true
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 
-BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 disable_dma32=on swinfo.fingerprint=sheng:13/V816.0.5.0.UNXMIXM:user mtdoops.fingerprint=sheng:13/V816.0.5.0.UNXMIXM:user
+BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 disable_dma32=on swinfo.fingerprint=$(LMODROID_VERSION) mtdoops.fingerprint=$(LMODROID_VERSION)
 BOARD_BOOTCONFIG := androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3 androidboot.console=ttyMSM0
 BOARD_BOOTCONFIG += androidboot.selinux=permissive
 
